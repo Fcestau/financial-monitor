51d0e1f41a58cfd043c805187d3bd3bf5a09ddd7
marezelej <marlej1997@gmail.com>
2021-10-01 04:28:11 -0300

    Finish CS-18_accounts_controller

---
+++ Fuentes/Servidor/messirveinvertir-be/database/migrations/1633042694312_accounts.ts
@@ -0,0 +1,30 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import { AccountType } from 'App/Models/Account'
+
+export default class Accounts extends BaseSchema {
+  protected tableName = 'accounts'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('uid').notNullable()
+      table.string('name').notNullable()
+      table.json('data').notNullable()
+      table.enum('type', [AccountType.IOL, AccountType.Manual], {
+        useNative: true,
+        enumName: 'account_type',
+        existingType: false,
+      })
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
