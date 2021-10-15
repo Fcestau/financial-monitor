a0b6decf234868f999a66ad5f317807f1ab71bf5
Felipe <cestaufelipe08@gmail.com>
2021-10-15 19:58:10 -0300

    refactor: add css classes to menu

--- Fuentes/Cliente/messirveinvertir-fe/src/components/general/Menu.vue
+++ Fuentes/Cliente/messirveinvertir-fe/src/components/general/Menu.vue
@@ -2,70 +2,75 @@
   <ion-menu side="start" menu-id="custom" class="my-custom-menu" content-id="main" color="tertiary">
     <ion-header>
       <ion-toolbar color="tertiary">
-        <ion-title class="ion-text-center">MESSIRVEINVERTIR</ion-title>
+        <ion-title class="ion-text-center ion-text-uppercase">{{ $t('menu.messirveinverir') }}</ion-title>
       </ion-toolbar>
-      <ion-avatar style="margin: 0 auto; margin-top: 20px; margin-bottom: 5px;">
-         <img src="https://pbs.twimg.com/media/EUCh095XQAIXfbM.jpg">
-      </ion-avatar>
-      <div class="ion-text-center" style="margin-top: 5px; margin-bottom: 100px; color:white;">
-        <ion-label>Rodrigo Mignola</ion-label>
-      </div>
+      <ion-card>
+        <ion-avatar class="avatar">
+          <img src="https://pbs.twimg.com/media/EUCh095XQAIXfbM.jpg">
+        </ion-avatar>
+        <div class="ion-text-center user-name">
+          <ion-label>Rodrigo Mignola</ion-label>
+        </div>
+      </ion-card>
+      
     </ion-header>
     <ion-content color="tertiary">
       <ion-list class="ion-list-container">
-          <ion-item color="tertiary" href=""><div style="margin: 0 auto;">LISTADO DE CUENTAS</div></ion-item>
-          <ion-item color="tertiary" href=""><div style="margin: 0 auto;">CARTERA</div></ion-item>
-          <ion-item color="tertiary" href="/chart"><div style="margin: 0 auto;">GRAFICAS</div></ion-item>
-          <ion-item color="tertiary" href=""><div style="margin: 0 auto;">HISTORIAL DE OPERACIONES</div></ion-item>
-          <ion-item color="tertiary" href="/barcode-scanner"><div style="margin: 0 auto;">ESCANEAR CÓDIGO QR</div></ion-item>
-          <ion-item color="tertiary" href=""><div style="margin: 0 auto;">ALERTAS ACTIVADAS</div></ion-item>
+          <ion-item color="tertiary" href="/external-accounts"><div style="">{{ $t('menu.account_listing') }}</div></ion-item>
+          <ion-item color="tertiary" href=""><div >{{ $t('menu.wallet') }}</div></ion-item>
+          <ion-item color="tertiary" href="/chart"><div >{{ $t('menu.charts') }}</div></ion-item>
+          <ion-item color="tertiary" href=""><div >{{ $t('menu.operation_history') }}</div></ion-item>
+          <ion-item color="tertiary" href="/barcode-scanner"><div >{{ $t('menu.scan_qr_code') }}</div></ion-item>
+          <ion-item color="tertiary" href=""><div >{{ $t('menu.active_alerts') }}</div></ion-item>
       </ion-list>
     </ion-content>
-    <div class="ion-text-center" style="margin-top: 80px; margin-bottom: 30px;">
-        <ion-button>SALIR</ion-button>
+    <div class="ion-text-center logout">
+        <ion-text>{{ $t('menu.logout')}}</ion-text>
     </div>
   </ion-menu>
 
   <ion-router-outlet id="main"></ion-router-outlet>
 </template>
-<style>
+<style scoped>
 .my-custom-menu {
-  --width: 350px;
   --background: #3444e4;
-  
+}
+
+.avatar {
+  margin: 0 auto; 
+  margin-top: 20px; 
+  margin-bottom: 5px;
+  width: 80px;
+  height: 80px;
+  color: black
+}
+
+.user-name {
+  margin-top: 5px; 
+  margin-bottom: 100px; 
+  color:white;
+}
+
+.logout {
+  margin-top: 80px; 
+  margin-bottom: 30px;
+  color:white;
 }
 
 .ion-list-container{
   padding-bottom: 0px;
   padding-top: 0px;
 }
+
+ion-item {
+  margin: 0 auto;
+}
 </style>
 
 <script>
-import { 
-  IonContent, 
-  IonHeader, 
-  IonItem, 
-  IonList, 
-  IonMenu, 
-  IonRouterOutlet,
-  IonTitle, 
-  IonToolbar,
-  menuController,
-  IonAvatar
-} from '@ionic/vue';
-import { defineComponent } from 'vue';
-
-export default defineComponent({
+import {menuController} from '@ionic/vue';
+export default ({
   components: {
-    IonContent, 
-    IonHeader, 
-    IonItem, 
-    IonList, 
-    IonMenu, 
-    IonRouterOutlet,
-    IonTitle, 
-    IonToolbar
   },
   methods: {
     openFirst() {
