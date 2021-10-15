ead7b96d7c6060e370d296e3db85e85a4a184e05
Felipe Cestau <cestaufelipe08@gmail.com>
2021-10-15 22:18:09 +0000

    CS-34

---
+++ Fuentes/Cliente/messirveinvertir-fe/src/components/business/Modals/TheLinkExternalAccountFormModal.vue
@@ -0,0 +1,65 @@
+<template >
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="end">
+        <ion-button size="small" @click="closeModal()">
+          <ion-icon slot="icon-only" :icon="closeOutline"></ion-icon>
+        </ion-button>
+      </ion-buttons>
+        <ion-title>{{ title }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+        <ion-row>
+          <ion-col size="=12">
+            <p class="info-text">{{ $t('external.complete_form') }}</p>
+              <TheTextInput v-model="form.email" label="email" type="email"></TheTextInput>
+              <TheTextInput v-model="form.password" label="password" type="password"></TheTextInput>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col size="12" class="ion-text-center">
+            <TheButton @click="submitForm" expand="block" icon="closeOutline" inner-text="confirm" class="ion-margin-top"></TheButton>
+          </ion-col>
+        </ion-row>
+    </ion-content>  
+</template>
+<script>
+import { modalController } from '@ionic/vue';
+import { closeOutline } from 'ionicons/icons';
+export default ({
+  name: 'TheLinkExternalAccountFormDialog',
+  props: {
+    title: { 
+      type: String, 
+      default: 'Super Modal' 
+    }
+  },
+  data() {
+    return {
+      form: {
+          email: null,
+          password: null,
+      }
+    }
+  },
+  setup() {
+    return {
+      closeOutline,
+    };
+  },
+  methods: {
+    closeModal() {
+      modalController.dismiss()
+    },
+    submitForm() {
+      console.log(this.form)
+    }
+  },
+});
+</script>
+<style scoped>
+.info-text {
+  font-size:12px;
+}
+</style>
---
+++ Fuentes/Cliente/messirveinvertir-fe/src/components/business/items/TheExternalAccountItem.vue
@@ -0,0 +1,49 @@
+<template>
+    <ion-item>
+      <ion-avatar slot="start">
+        <img :src="item.avatar" />
+      </ion-avatar>
+      <div class="item">
+        <ion-label>
+          <h3>{{ item.title }}</h3>
+        </ion-label>
+        <ion-note v-if="showAmount" :color="item.color">
+          {{ item.assetType }} {{ item.amount }}
+        </ion-note>
+      </div>         
+      <ion-buttons>
+        <ion-button size="small" @click="action()">
+          <ion-icon slot="icon-only" :icon="actionIcon"></ion-icon>
+        </ion-button>
+      </ion-buttons>
+    </ion-item>
+</template>
+<script>
+export default ({
+    props: {
+        item: {
+            type: Object,
+            required: true
+        },
+        showAmount: {
+            type: Boolean,
+            required: true
+        },
+        actionIcon: {
+            type: String,
+            required: false
+        }
+    },
+    methods: {
+        action() {
+            this.$emit('selectedItem');
+        }
+    },
+
+})
+</script>
+<style scoped>
+.item {
+  width: -webkit-fill-available;
+}
+</style>
