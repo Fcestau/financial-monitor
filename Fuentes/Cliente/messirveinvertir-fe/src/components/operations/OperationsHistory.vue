e92f9d869ed779410b9d7aa20877963997f8e6b3
franco <francomiret11@gmail.com>
2021-10-30 15:37:07 -0300

    refactor: change href to router-link

--- Fuentes/Cliente/messirveinvertir-fe/src/components/operations/OperationsHistory.vue
+++ Fuentes/Cliente/messirveinvertir-fe/src/components/operations/OperationsHistory.vue
@@ -4,60 +4,106 @@
       <ion-buttons slot="start">
         <ion-menu-button auto-hide="false" />
       </ion-buttons>
-      <ion-title class="c-title"> {{ $t('operations.operations_history') }} </ion-title>
+      <ion-title class="c-title">
+        {{ $t('operations.operations_history') }}
+      </ion-title>
     </ion-toolbar>
     <ion-content class="ion-padding">
-     <TheButton  href="operation-creator" expand="block" inner-text="operations.new_operation" />
-     <ion-row class="ion-text-center ion-margin-top ion-margin-bottom">
-         <ion-col>
-            <ion-chip :disabled="noItems" @click="filterByPeriod('1_day')" color="tertiary">
-              <ion-label class="font-bold"  color="tertiary">{{ $t('operations.time_filter.1_day') }}</ion-label>
-            </ion-chip>
-         </ion-col>
-         <ion-col>
-            <ion-chip :disabled="noItems" @click="filterByPeriod('1_week')" color="tertiary">
-              <ion-label class="font-bold" color="tertiary">{{ $t('operations.time_filter.1_week') }}</ion-label>
-            </ion-chip>
-         </ion-col>
-         <ion-col>
-            <ion-chip :disabled="noItems" @click="filterByPeriod('1_month')" color="tertiary">
-              <ion-label class="font-bold" color="tertiary">{{ $t('operations.time_filter.1_month') }}</ion-label>
-            </ion-chip>
-         </ion-col>
-         <ion-col>
-            <ion-chip :disabled="noItems" @click="filterByPeriod('3_months')" color="tertiary">
-              <ion-label class="font-bold" color="tertiary">{{ $t('operations.time_filter.3_months') }}</ion-label>
-            </ion-chip>         
-          </ion-col>
-         <ion-col>
-            <ion-chip :disabled="noItems" @click="filterByPeriod('1_year')" color="tertiary">
-              <ion-label class="font-bold" color="tertiary">{{ $t('operations.time_filter.1_year') }}</ion-label>
-            </ion-chip>
-         </ion-col>
-     </ion-row>
-     <template  v-if="items">
-       <template v-for="(item, index) in items" :key="index">
-          <ion-item-divider class="divider ion-padding-top ion-padding-bottom"
-           v-if="index == 0">
+      <TheButton
+        router-link="operation-creator"
+        expand="block"
+        inner-text="operations.new_operation"
+      />
+      <ion-row class="ion-text-center ion-margin-top ion-margin-bottom">
+        <ion-col>
+          <ion-chip
+            :disabled="noItems"
+            @click="filterByPeriod('1_day')"
+            color="tertiary"
+          >
+            <ion-label class="font-bold" color="tertiary">{{
+              $t('operations.time_filter.1_day')
+            }}</ion-label>
+          </ion-chip>
+        </ion-col>
+        <ion-col>
+          <ion-chip
+            :disabled="noItems"
+            @click="filterByPeriod('1_week')"
+            color="tertiary"
+          >
+            <ion-label class="font-bold" color="tertiary">{{
+              $t('operations.time_filter.1_week')
+            }}</ion-label>
+          </ion-chip>
+        </ion-col>
+        <ion-col>
+          <ion-chip
+            :disabled="noItems"
+            @click="filterByPeriod('1_month')"
+            color="tertiary"
+          >
+            <ion-label class="font-bold" color="tertiary">{{
+              $t('operations.time_filter.1_month')
+            }}</ion-label>
+          </ion-chip>
+        </ion-col>
+        <ion-col>
+          <ion-chip
+            :disabled="noItems"
+            @click="filterByPeriod('3_months')"
+            color="tertiary"
+          >
+            <ion-label class="font-bold" color="tertiary">{{
+              $t('operations.time_filter.3_months')
+            }}</ion-label>
+          </ion-chip>
+        </ion-col>
+        <ion-col>
+          <ion-chip
+            :disabled="noItems"
+            @click="filterByPeriod('1_year')"
+            color="tertiary"
+          >
+            <ion-label class="font-bold" color="tertiary">{{
+              $t('operations.time_filter.1_year')
+            }}</ion-label>
+          </ion-chip>
+        </ion-col>
+      </ion-row>
+      <template v-if="items">
+        <template v-for="(item, index) in items" :key="index">
+          <ion-item-divider
+            class="divider ion-padding-top ion-padding-bottom"
+            v-if="index == 0"
+          >
             <ion-label>
               <span>{{ dateLabel(item.fecha) }}</span>
             </ion-label>
           </ion-item-divider>
           <TheOperationItem
-                :item="item"
-                :action-icon="trashOutline"
-                :can-select="item.accountType === 'Manual'"
-                @selectedItem="deleteItem(item)"
-            />
-          <ion-item-divider class="divider ion-padding-top ion-padding-bottom"
-           v-if="items.length > index+1 && dateLabel(items[index].fecha) != dateLabel(items[index+1].fecha)">
+            :item="item"
+            :action-icon="trashOutline"
+            :can-select="item.accountType === 'Manual'"
+            @selectedItem="deleteItem(item)"
+          />
+          <ion-item-divider
+            class="divider ion-padding-top ion-padding-bottom"
+            v-if="
+              items.length > index + 1 &&
+                dateLabel(items[index].fecha) !=
+                  dateLabel(items[index + 1].fecha)
+            "
+          >
             <ion-label>
-              <span v-if="items[index+1]">{{ dateLabel(items[index+1].fecha) }}</span>
+              <span v-if="items[index + 1]">{{
+                dateLabel(items[index + 1].fecha)
+              }}</span>
             </ion-label>
           </ion-item-divider>
-       </template>   
-     </template>
-     <NoInformationCard v-if="noItems" />
+        </template>
+      </template>
+      <NoInformationCard v-if="noItems" />
     </ion-content>
   </ion-page>
 </template>
@@ -83,8 +129,7 @@ export default {
           assetType: 'USD',
           amount: 300,
           type: 'Venta',
-          fecha: '06/05/2021:18:20'
-
+          fecha: '06/05/2021:18:20',
         },
         {
           title: 'Binance',
@@ -103,7 +148,7 @@ export default {
           assetType: 'ETH',
           amount: 1,
           type: 'Compra',
-          fecha: '18/06/2021:16:50'
+          fecha: '18/06/2021:16:50',
         },
         {
           title: 'buenbit',
@@ -113,7 +158,7 @@ export default {
           assetType: 'USD',
           amount: 300,
           type: 'Compra',
-          fecha: '09/07/2021:12:30'
+          fecha: '09/07/2021:12:30',
         },
         {
           title: 'Invertir Online',
@@ -123,8 +168,7 @@ export default {
           assetType: 'USD',
           amount: 300,
           type: 'Venta',
-          fecha: '03/07/2021:13:28'
-
+          fecha: '03/07/2021:13:28',
         },
         {
           title: 'Binance',
@@ -143,7 +187,7 @@ export default {
           assetType: 'ETH',
           amount: 1,
           type: 'Compra',
-          fecha: '15/07/2021:19:08'
+          fecha: '15/07/2021:19:08',
         },
         {
           title: 'buenbit',
@@ -153,15 +197,15 @@ export default {
           assetType: 'USD',
           amount: 300,
           type: 'Compra',
-          fecha: '15/07/2021:21:40'
+          fecha: '15/07/2021:21:40',
         },
       ],
     };
   },
   computed: {
     noItems() {
-      return this.items.length === 0
-    }
+      return this.items.length === 0;
+    },
   },
   mounted() {
     this.items.forEach((element) => {
@@ -175,10 +219,10 @@ export default {
   methods: {
     dateLabel(date) {
       //TODO: realizar el formateo correcto
-     return this.period === '1_day' ? date.slice(11,16) : date.slice(0,10) 
+      return this.period === '1_day' ? date.slice(11, 16) : date.slice(0, 10);
     },
     filterByPeriod(value) {
-      this.period = value
+      this.period = value;
     },
     deleteItem(item) {
       this.deleteItemAlertConfirm(item);
@@ -208,12 +252,12 @@ export default {
       });
       return alert.present();
     },
-  }
-}
+  },
+};
 </script>
 <style scoped>
 .c-title {
-    width: fit-content;
+  width: fit-content;
 }
 .font-bold {
   font-weight: bold;
@@ -224,7 +268,7 @@ export default {
   font-size: 14px;
   color: grey;
   display: grid;
-  opacity: .6;
+  opacity: 0.6;
   justify-content: center;
 }
 </style>
