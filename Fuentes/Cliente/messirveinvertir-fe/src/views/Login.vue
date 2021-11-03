dc260349aabd0d7322e37490e696a55f8a34c79e
franco <francomiret11@gmail.com>
2021-11-03 20:35:45 -0300

    feat: add inputs to login

--- Fuentes/Cliente/messirveinvertir-fe/src/views/Login.vue
+++ Fuentes/Cliente/messirveinvertir-fe/src/views/Login.vue
@@ -1,5 +1,58 @@
 <template>
-  <div>
-    Login
-  </div>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title> MESSIRVEINVERTIR </ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content class="ion-padding">
+      <TheTextInput
+        v-model="form.email"
+        label="email"
+        type="email"
+      ></TheTextInput>
+      <TheTextInput
+        v-model="form.password"
+        label="password"
+        type="password"
+      ></TheTextInput>
+
+      <ion-button
+        expand="block"
+        inner-text="confirm"
+        class="ion-margin-top"
+        v-on:click="login(form)"
+      >
+        Login
+      </ion-button>
+    </ion-content>
+  </ion-page>
 </template>
+<script>
+import TokenStore from '@/store/tokenStore';
+import LoginApiService from '@/services/loginApiService';
+
+export default {
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    async login(form) {
+      try {
+        const token = await LoginApiService.login(form);
+        TokenStore.save(token);
+        this.$router.push('/');
+        console.log('se logeo perro');
+      } catch (exception) {
+        this.loginError = exception;
+      }
+    },
+  },
+};
+</script>
