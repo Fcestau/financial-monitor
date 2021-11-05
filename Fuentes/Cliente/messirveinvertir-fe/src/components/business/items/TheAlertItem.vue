b7b77d64bedb50df0a6756f511b068bdd71acaf7
franco <francomiret11@gmail.com>
2021-11-05 19:03:27 -0300

    refactor: add alert when delete and refactor subtitle card

--- Fuentes/Cliente/messirveinvertir-fe/src/components/business/items/TheAlertItem.vue
+++ Fuentes/Cliente/messirveinvertir-fe/src/components/business/items/TheAlertItem.vue
@@ -2,19 +2,22 @@
   <ion-card>
     <ion-item>
       <ion-avatar slot="start">
-        <img
-          :src="item.account.avatar"
-        />
+        <img :src="item.account.avatar" />
       </ion-avatar>
       <ion-card-content>
-        <ion-card-subtitle>El precio cambio {{ item.hourlyDeltaVolume }}%</ion-card-subtitle>
+        <ion-card-subtitle v-if="item.hourlyDeltaVolume !== null"
+          >El volumen cambio {{ item.hourlyDeltaVolume }}%</ion-card-subtitle
+        >
+        <ion-card-subtitle v-if="item.hourlyDeltaPrice !== null"
+          >El precio cambio {{ item.hourlyDeltaPrice }}%</ion-card-subtitle
+        >
         <ion-badge style="margin-right:5px" color="danger" mode="ios">{{
           item.asset.name
         }}</ion-badge>
         <ion-badge color="primary" mode="ios">{{ item.frequency }}</ion-badge>
       </ion-card-content>
       <ion-buttons slot="end">
-        <ion-button  size="small" @click="action()">
+        <ion-button size="small" @click="action()">
           <ion-icon slot="icon-only" :icon="actionIcon"></ion-icon>
         </ion-button>
       </ion-buttons>
