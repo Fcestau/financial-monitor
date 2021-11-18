3b7e6e1c2ff1a11925fefea1249a70b6d0db6dba
marezelej <marlej1997@gmail.com>
2021-11-17 21:18:47 -0300

    feat(CS-75): GET and CREATE assets methods

--- Fuentes/Servidor/messirveinvertir-be/start/routes/api/v1/assets.ts
+++ Fuentes/Servidor/messirveinvertir-be/start/routes/api/v1/assets.ts
@@ -1,6 +1,8 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
+  Route.post('', 'AssetsController.createAssets')
+  Route.get('', 'AssetsController.listAssets')
   Route.get('stock', 'AssetsController.getStock')
   Route.post('fetch', 'AssetsController.fetchAssets')
 })
