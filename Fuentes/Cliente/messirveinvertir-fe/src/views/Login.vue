37deb474dc6db08be1d3a64ab4c0fdfac5b1aeda
Felipe <cestaufelipe08@gmail.com>
2021-11-19 19:52:53 -0300

    feat: add form validation in login and register

--- Fuentes/Cliente/messirveinvertir-fe/src/views/Login.vue
+++ Fuentes/Cliente/messirveinvertir-fe/src/views/Login.vue
@@ -8,31 +8,41 @@
         ></ion-img>
       </div>
       <p class="ion-text-center ion-padding">{{ $t('login_phrase') }}</p>
-      <TheTextInput
-        v-model="form.email"
-        label="email"
-        type="email"
-      ></TheTextInput>
-      <TheTextInput
-        v-model="form.password"
-        label="password"
-        type="password"
-      ></TheTextInput>
-      <TheButton
-        @click="login(form)"
-        expand="block"
-        icon="closeOutline"
-        inner-text="login"
-        class="ion-margin-top"
-      ></TheButton>
-      <TheButton
-        href="register"
-        expand="block"
-        icon="closeOutline"
-        inner-text="signin"
-        color="secondary"
-        class="ion-margin-top"
-      ></TheButton>
+      <Form @submit="login">
+        <Field name="email" v-slot="{ field }" :rules="isRequired">
+          <TheTextInput
+            v-bind="field"
+            label="email"
+            type="email"
+            name="email"
+          ></TheTextInput>
+        </Field>
+        <ErrorMessage class="error" name="email" />
+        <Field name="password" v-slot="{ field }" :rules="isRequired">
+          <TheTextInput
+            v-bind="field"
+            label="password"
+            type="password"
+            name="password"
+          ></TheTextInput>
+        </Field>
+        <ErrorMessage class="error" name="password" />
+        <TheButton
+          type="submit"
+          expand="block"
+          icon="closeOutline"
+          inner-text="login"
+          class="ion-margin-top"
+        ></TheButton>
+        <TheButton
+          href="register"
+          expand="block"
+          icon="closeOutline"
+          inner-text="signin"
+          color="secondary"
+          class="ion-margin-top"
+        ></TheButton>
+      </Form>
     </ion-content>
   </ion-page>
 </template>
@@ -46,24 +56,26 @@
   flex-wrap: nowrap;
   justify-content: center;
 }
+.error {
+  font-size: 12px;
+  color: #eb3333;
+}
 </style>
 <script>
 import { FCM } from "@capacitor-community/fcm";
 import TokenStore from '@/store/tokenStore';
 import authApiService from '@/services/authApiService';
+import { Field, Form, ErrorMessage } from 'vee-validate';
 export default {
-  data() {
-    return {
-      form: {
-        email: '',
-        password: '',
-      },
-    };
+  components: {
+    Field,
+    Form,
+    ErrorMessage,
   },
   methods: {
-    async login(form) {
+    async login(data) {
       try {
-        const token = await authApiService.login(form);
+        const token = await authApiService.login(data);
         TokenStore.save(token.data.token);
         const user = await authApiService.user(token.data.token)
         FCM.subscribeTo({ topic: user.data.uid })
@@ -72,6 +84,9 @@ export default {
       } catch (exception) {
         this.loginError = exception;
       }
+    },
+    isRequired(value) {
+      return value ? true : this.$t('validation.required');
     },
   },
 };
